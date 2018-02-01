da3ba27b940f29d1993c45d323993db9dba3f0aa
yjyvi <yjyvi110@gmail.com>
2018-02-02 00:40:00 +0800

    完善分类模块及公共标题栏XML

--- app/src/main/java/com/risenb/wette/ui/classify/ClassifyFragment.java
+++ app/src/main/java/com/risenb/wette/ui/classify/ClassifyFragment.java
@@ -1,13 +1,13 @@
 package com.risenb.wette.ui.classify;
 
 import android.os.Bundle;
-import android.support.v7.widget.DividerItemDecoration;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.risenb.wette.R;
@@ -15,6 +15,7 @@ import com.risenb.wette.adapter.type.ClassifyLeftAdapter;
 import com.risenb.wette.adapter.type.ClassifyRightAdapter;
 import com.risenb.wette.ui.LazyLoadFragment;
 import com.risenb.wette.utils.ToastUtils;
+import com.risenb.wette.views.SmoothScrollLayoutManager;
 
 import org.xutils.view.annotation.ViewInject;
 
@@ -29,11 +30,11 @@ public class ClassifyFragment extends LazyLoadFragment {
     @ViewInject(R.id.rv_left)
     private RecyclerView rv_left;
 
-    @ViewInject(R.id.rv_right)
-    private RecyclerView rv_right;
     public ArrayList<String> mLeftData;
     private ArrayList<String> mRightData;
     public ClassifyRightAdapter mClassifyRightAdapter;
+    private ArrayList<TwoClassifyFragment.Category> itemList  =new ArrayList<>();
+    private TwoClassifyFragment fragment;
 
     @Override
     protected void loadViewLayout(LayoutInflater inflater, ViewGroup container) {
@@ -55,32 +56,38 @@ public class ClassifyFragment extends LazyLoadFragment {
         rightData();
 
         //左侧列表
-        LinearLayoutManager layout = new LinearLayoutManager(getActivity());
+        SmoothScrollLayoutManager layout = new SmoothScrollLayoutManager(getActivity());
         layout.setAutoMeasureEnabled(true);
+
         rv_left.setLayoutManager(layout);
         ClassifyLeftAdapter classifyLeftAdapter = new ClassifyLeftAdapter(R.layout.item_classify_left, mLeftData);
         rv_left.setAdapter(classifyLeftAdapter);
-        rv_left.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
         classifyLeftAdapter.setOnItemClickListener(new BaseQuickAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
-
                 scroll(position,adapter,view);
-                TextView item = (TextView) adapter.getItem(position);
-                item.setSelected(true);
                 ToastUtils.showToast(String.valueOf(position));
             }
 
         });
 
+        initRightData();
+    }
 
-        //右则分类列表
-        LinearLayoutManager layout2 = new LinearLayoutManager(getActivity());
-        layout2.setAutoMeasureEnabled(true);
-        rv_right.setLayoutManager(layout2);
-        mClassifyRightAdapter = new ClassifyRightAdapter(R.layout.item_classify_right, mRightData);
-        rv_right.setAdapter(mClassifyRightAdapter);
-        rv_right.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
+    private void initRightData() {
+        for(int i=1; i<20; i++)
+        {
+            itemList.add(new TwoClassifyFragment.Category("选项 " + i, ""+i));
+        }
+
+        //模拟右侧标签页
+        fragment = new TwoClassifyFragment();
+        Bundle bundle = new Bundle();
+        bundle.putString("name", "c1");
+        fragment.setArguments(bundle);
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.ll_right_main, fragment, "c0").commit();
 
     }
 
@@ -104,10 +111,8 @@ public class ClassifyFragment extends LazyLoadFragment {
         }
 
         RecyclerView.LayoutManager layoutManager  = rv_left.getLayoutManager();
-
         if (layoutManager instanceof LinearLayoutManager) {
             LinearLayoutManager linearManager = (LinearLayoutManager) layoutManager;
-
             //上移
             if (position <= (linearManager.findFirstVisibleItemPosition() + ce)) {
                 rv_left.smoothScrollToPosition(position - ce);
@@ -123,14 +128,10 @@ public class ClassifyFragment extends LazyLoadFragment {
 
             lastPosition = position;
 
-//            adapter.setSelected(position);
-//            adapter.notifyDataSetChanged();
         }
 
-
-
         //更新右侧标签页的标题
-//        fragment.updateTitle("c" + (position + 1));
+        fragment.updateTitle("c" + (position + 1));
     }
 
     private void leftData() {
---
+++ app/src/main/java/com/risenb/wette/views/SmoothScrollLayoutManager.java
@@ -0,0 +1,34 @@
+package com.risenb.wette.views;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.LinearSmoothScroller;
+import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
+
+/**
+ * Created by yjyvi on 2018/2/1.
+ */
+
+public class SmoothScrollLayoutManager extends LinearLayoutManager {
+    public SmoothScrollLayoutManager(Context context) {
+        super(context);
+    }
+
+    @Override
+    public void smoothScrollToPosition(RecyclerView recyclerView,
+                                       RecyclerView.State state, final int position) {
+
+        LinearSmoothScroller smoothScroller =
+                new LinearSmoothScroller(recyclerView.getContext()) {
+                    // 返回：滑过1px时经历的时间(ms)。
+                    @Override
+                    protected float calculateSpeedPerPixel(DisplayMetrics displayMetrics) {
+                        return 150f / displayMetrics.densityDpi;
+                    }
+                };
+
+        smoothScroller.setTargetPosition(position);
+        startSmoothScroll(smoothScroller);
+    }
+}
