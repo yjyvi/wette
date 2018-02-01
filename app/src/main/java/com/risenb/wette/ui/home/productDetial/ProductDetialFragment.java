0950bc8e492ebdbbcb525e8925cf8d045358ac54
yjyvi <yjyvi110@gmail.com>
2018-02-01 19:06:54 +0800

    修改详情页面

--- app/src/main/java/com/risenb/wette/ui/home/productDetial/ProductDetialFragment.java
+++ app/src/main/java/com/risenb/wette/ui/home/productDetial/ProductDetialFragment.java
@@ -2,8 +2,13 @@ package com.risenb.wette.ui.home.productDetial;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import com.risenb.wette.R;
 import com.risenb.wette.ui.LazyLoadFragment;
@@ -17,7 +22,10 @@ import org.xutils.view.annotation.ViewInject;
 public class ProductDetialFragment extends LazyLoadFragment {
 
     @ViewInject(R.id.web_content)
-    private WebView web_content;
+    private WebView wb_content;
+
+    @ViewInject(R.id.pb_loading)
+    private ProgressBar pb_loading;
 
     @Override
     protected void loadViewLayout(LayoutInflater inflater, ViewGroup container) {
@@ -32,6 +40,41 @@ public class ProductDetialFragment extends LazyLoadFragment {
 
     @Override
     protected void prepareData() {
+
+        WebSettings settings = wb_content.getSettings();
+        settings.setSupportZoom(false);
+
+        settings.setJavaScriptEnabled(true);
+        wb_content.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+        });
+        settings.setDefaultTextEncodingName("utf-8");
+        wb_content.setWebChromeClient(new WebChromeClient(){
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+
+                if(newProgress==100){
+                    //加载完网页进度条消失
+                    pb_loading.setVisibility(View.GONE);
+                }
+                else{
+                    //开始加载网页时显示进度条
+                    pb_loading.setVisibility(View.VISIBLE);
+                    //设置进度值
+                    pb_loading.setProgress(newProgress);
+                }
+            }
+        });
+
+
+        wb_content.loadUrl("http://www.baidu.com");
 
     }
 
